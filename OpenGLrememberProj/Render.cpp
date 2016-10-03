f07dc3b9b84e72021ce727e5891a6da675487e8b
Andrey Gavrilov <faktorial5@gmail.com>
2016-10-03 11:12:46 +0300

    Лаба 2 по графике

---
+++ OpenGLrememberProj/Render.cpp
@@ -0,0 +1,381 @@
+#include "Render.h"
+
+#include <windows.h>
+#include <GL\GL.h>
+#include <GL\GLU.h>
+
+#include "MyOGL.h"
+
+#include "Camera.h"
+#include "Light.h"
+#include "Primitives.h"
+
+
+
+bool textureMode = true;
+bool lightMode = true;
+
+//класс для настройки камеры
+class CustomCamera : public Camera
+{
+public:
+	//дистанция камеры
+	double camDist;
+	//углы поворота камеры
+	double fi1, fi2;
+
+	
+	//значния масеры по умолчанию
+	CustomCamera()
+	{
+		camDist = 15;
+		fi1 = 1;
+		fi2 = 1;
+	}
+
+	
+	//считает позицию камеры, исходя из углов поворота, вызывается движком
+	void SetUpCamera()
+	{
+		//отвечает за поворот камеры мышкой
+		lookPoint.setCoords(0, 0, 0);
+
+		pos.setCoords(camDist*cos(fi2)*cos(fi1),
+			camDist*cos(fi2)*sin(fi1),
+			camDist*sin(fi2));
+
+		if (cos(fi2) <= 0)
+			normal.setCoords(0, 0, -1);
+		else
+			normal.setCoords(0, 0, 1);
+
+		LookAt();
+	}
+
+	void CustomCamera::LookAt()
+	{
+		//функция настройки камеры
+		gluLookAt(pos.X(), pos.Y(), pos.Z(), lookPoint.X(), lookPoint.Y(), lookPoint.Z(), normal.X(), normal.Y(), normal.Z());
+	}
+
+
+
+}  camera;   //создаем объект камеры
+
+
+//Класс для настройки света
+class CustomLight : public Light
+{
+public:
+	CustomLight()
+	{
+		//начальная позиция света
+		pos = Vector3(1, 1, 3);
+	}
+
+	
+	//рисует сферу и линии под источником света, вызывается движком
+	void  DrawLightGhismo()
+	{
+		glDisable(GL_LIGHTING);
+
+		
+		glColor3d(0.9, 0.8, 0);
+		Sphere s;
+		s.pos = pos;
+		s.scale = s.scale*0.08;
+		s.Show();
+		
+		if (OpenGL::isKeyPressed('G'))
+		{
+			glColor3d(0, 0, 0);
+			//линия от источника света до окружности
+			glBegin(GL_LINES);
+			glVertex3d(pos.X(), pos.Y(), pos.Z());
+			glVertex3d(pos.X(), pos.Y(), 0);
+			glEnd();
+
+			//рисуем окруность
+			Circle c;
+			c.pos.setCoords(pos.X(), pos.Y(), 0);
+			c.scale = c.scale*1.5;
+			c.Show();
+		}
+
+	}
+
+	void SetUpLight()
+	{
+		GLfloat amb[] = { 0.2, 0.2, 0.2, 0 };
+		GLfloat dif[] = { 1.0, 1.0, 1.0, 0 };
+		GLfloat spec[] = { .7, .7, .7, 0 };
+		GLfloat position[] = { pos.X(), pos.Y(), pos.Z(), 1. };
+
+		// параметры источника света
+		glLightfv(GL_LIGHT0, GL_POSITION, position);
+		// характеристики излучаемого света
+		// фоновое освещение (рассеянный свет)
+		glLightfv(GL_LIGHT0, GL_AMBIENT, amb);
+		// диффузная составляющая света
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, dif);
+		// зеркально отражаемая составляющая света
+		glLightfv(GL_LIGHT0, GL_SPECULAR, spec);
+
+		glEnable(GL_LIGHT0);
+	}
+
+
+} light;  //создаем источник света
+
+
+
+
+//старые координаты мыши
+int mouseX = 0, mouseY = 0;
+
+void mouseEvent(OpenGL *ogl, int mX, int mY)
+{
+	int dx = mouseX - mX;
+	int dy = mouseY - mY;
+	mouseX = mX;
+	mouseY = mY;
+
+	//меняем углы камеры при нажатой левой кнопке мыши
+	if (OpenGL::isKeyPressed(VK_RBUTTON))
+	{
+		camera.fi1 += 0.01*dx;
+		camera.fi2 += -0.01*dy;
+	}
+
+	
+	//двигаем свет по плоскости, в точку где мышь
+	if (OpenGL::isKeyPressed('G') && !OpenGL::isKeyPressed(VK_LBUTTON))
+	{
+		LPPOINT POINT = new tagPOINT();
+		GetCursorPos(POINT);
+		ScreenToClient(ogl->getHwnd(), POINT);
+		POINT->y = ogl->getHeight() - POINT->y;
+
+		Ray r = camera.getLookRay(POINT->x, POINT->y);
+
+		double z = light.pos.Z();
+
+		double k = 0, x = 0, y = 0;
+		if (r.direction.Z() == 0)
+			k = 0;
+		else
+			k = (z - r.origin.Z()) / r.direction.Z();
+
+		x = k*r.direction.X() + r.origin.X();
+		y = k*r.direction.Y() + r.origin.Y();
+
+		light.pos = Vector3(x, y, z);
+	}
+
+	if (OpenGL::isKeyPressed('G') && OpenGL::isKeyPressed(VK_LBUTTON))
+	{
+		light.pos = light.pos + Vector3(0, 0, 0.02*dy);
+	}
+
+	
+}
+
+void mouseWheelEvent(OpenGL *ogl, int delta)
+{
+
+	if (delta < 0 && camera.camDist <= 1)
+		return;
+	if (delta > 0 && camera.camDist >= 100)
+		return;
+
+	camera.camDist += 0.01*delta;
+
+}
+
+void keyDownEvent(OpenGL *ogl, int key)
+{
+	if (key == 'L')
+	{
+		lightMode = !lightMode;
+	}
+
+	if (key == 'T')
+	{
+		textureMode = !textureMode;
+	}
+
+	if (key == 'R')
+	{
+		camera.fi1 = 1;
+		camera.fi2 = 1;
+		camera.camDist = 15;
+
+		light.pos = Vector3(1, 1, 3);
+	}
+
+	if (key == 'F')
+	{
+		light.pos = camera.pos;
+	}
+}
+
+void keyUpEvent(OpenGL *ogl, int key)
+{
+	
+}
+
+
+
+
+//выполняется перед первым рендером
+void initRender(OpenGL *ogl)
+{
+	//настройка текстур
+
+	//4 байта на хранение пикселя
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+
+	//настройка режима наложения текстур
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+	//включаем текстуры
+	glEnable(GL_TEXTURE_2D);
+	
+
+	//массив трехбайтных элементов  (R G B)
+	RGBTRIPLE *texarray;
+
+	//массив символов, (высота*ширина*4      4, потомучто   выше, мы указали использовать по 4 байта на пиксель текстуры - R G B A)
+	char *texCharArray;
+	int texW, texH;
+	OpenGL::LoadBMP("texture.bmp", &texW, &texH, &texarray);
+	OpenGL::RGBtoChar(texarray, texW, texH, &texCharArray);
+
+	
+	GLuint texId;
+	//генерируем ИД для текстуры
+	glGenTextures(1, &texId);
+	//биндим айдишник, все что будет происходить с текстурой, будте происходить по этому ИД
+	glBindTexture(GL_TEXTURE_2D, texId);
+
+	//загружаем текстуру в видеопямять, в оперативке нам больше  она не нужна
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texW, texH, 0, GL_RGBA, GL_UNSIGNED_BYTE, texCharArray);
+
+	//отчистка памяти
+	free(texCharArray);
+	free(texarray);
+
+	//наводим шмон
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+
+	//камеру и свет привязываем к "движку"
+	ogl->mainCamera = &camera;
+	ogl->mainLight = &light;
+
+	// нормализация нормалей : их длины будет равна 1
+	glEnable(GL_NORMALIZE);
+
+	// устранение ступенчатости для линий
+	glEnable(GL_LINE_SMOOTH); 
+
+
+	//   задать параметры освещения
+	//  параметр GL_LIGHT_MODEL_TWO_SIDE - 
+	//                0 -  лицевые и изнаночные рисуются одинаково(по умолчанию), 
+	//                1 - лицевые и изнаночные обрабатываются разными режимами       
+	//                соответственно лицевым и изнаночным свойствам материалов.    
+	//  параметр GL_LIGHT_MODEL_AMBIENT - задать фоновое освещение, 
+	//                не зависящее от сточников
+	// по умолчанию (0.2, 0.2, 0.2, 1.0)
+
+	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, 0);
+}
+
+
+
+
+
+void Render(OpenGL *ogl)
+{       	
+	
+	
+	
+	glDisable(GL_TEXTURE_2D);
+	glDisable(GL_LIGHTING);
+
+	glEnable(GL_DEPTH_TEST);
+	if (textureMode)
+		glEnable(GL_TEXTURE_2D);
+
+	if (lightMode)
+		glEnable(GL_LIGHTING);
+
+
+	//альфаналожение
+	//glEnable(GL_BLEND);
+	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
+	//настройка материала
+	GLfloat amb[] = { 0.2, 0.2, 0.1, 1. };
+	GLfloat dif[] = { 0.4, 0.65, 0.5, 1. };
+	GLfloat spec[] = { 0.9, 0.8, 0.3, 1. };
+	GLfloat sh = 0.1f * 256;
+
+
+	//фоновая
+	glMaterialfv(GL_FRONT, GL_AMBIENT, amb);
+	//дифузная
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, dif);
+	//зеркальная
+	glMaterialfv(GL_FRONT, GL_SPECULAR, spec);\
+	//размер блика
+	glMaterialf(GL_FRONT, GL_SHININESS, sh);
+
+    //чтоб было красиво, без квадратиков (сглаживание освещения)
+	glShadeModel(GL_SMOOTH);
+	//===================================
+	//Прогать тут  
+
+
+	//Начало рисования квадратика станкина
+	double A[2] = { -4, -4 };
+	double B[2] = { 4, -4 };
+	double C[2] = { 4, 4 };
+	double D[2] = { -4, 4 };
+
+	
+	
+	glBegin(GL_QUADS);
+
+	glNormal3d(0, 0, 1);
+	glTexCoord2d(0, 0);
+	glVertex2dv(A);
+	glTexCoord2d(1, 0);
+	glVertex2dv(B);
+	glTexCoord2d(1, 1);
+	glVertex2dv(C);
+	glTexCoord2d(0, 1);
+	glVertex2dv(D);
+
+	glEnd();
+	//конец рисования квадратика станкина
+    
+	
+	//текст сообщения вверху слева, если надоест - закоментировать, или заменить =)
+	char c[250];  //максимальная длина сообщения
+	sprintf_s(c, "(T)Текстуры - %d\n(L)Свет - %d\n\nУправление светом:\n"
+		"G - перемещение в горизонтальной плоскости,\nG+ЛКМ+перемещение по вертикальной линии\n"
+		"R - установить камеру и свет в начальное положение\n"
+		"F - переместить свет в точку камеры", textureMode, lightMode);
+	ogl->message = std::string(c);
+
+
+
+
+}   //конец тела функции
+
